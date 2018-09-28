1eaee0e771f185caabb8c14bab343a82bbe2c492
ggong <ggong__@nate.com>
2018-09-28 11:24:35 +0900

    HelloWorld

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,15 +1,15 @@
-﻿using System;
+﻿using System;                // using: 해당 네임스페이스의 클래스를 사용하겠다.
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HelloWorld
+namespace HelloWorld        // namespace: 분류이름
 {
-    class Program
+    class Program           // class: 설계도;규격정의
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args)         // static: 객체 생성없이 사용 가능
+        {                                       // Main: CLR이 실행
             Console.WriteLine("Hello C#");
         }
     }
