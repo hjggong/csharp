d07b73f09455b1080784979ee5983d8171b070c1
ggong <ggong__@nate.com>
2018-10-01 17:28:33 +0900

    day2

---
+++ Day2/Day2/Program11.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Day2
+{
+    class Program11
+    {
+        public static void Main()
+        {
+            //Hashtable onj = new Hashtable();
+            Dictionary<String, String> onj = new Dictionary<string, string>();
+
+            onj.Add("김길동", "서울");
+            onj.Add("홍길동", "광주");
+            onj.Add("박길동", "부산");
+
+            try
+            {
+                onj.Add("김길동", "서울");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("키값 중복!!");
+            }
+
+            Console.WriteLine("for key = \"name\", value={0}.", onj["홍길동"]);
+            onj["박길동"] = "제주";
+            Console.WriteLine("for key = \"name\", value={0}.", onj["박길동"]);
+
+            if (!onj.ContainsKey("최길동"))
+            {
+                onj.Add("최길동", "하와이");
+                Console.WriteLine("for key = \"who\", value={0}.", onj["최길동"]);
+            }
+            Console.WriteLine();
+
+            //foreach(DictionaryEntry d in onj)
+            foreach(KeyValuePair<string, string> d in onj)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}", d.Key, d.Value);
+            }
+
+            /*
+            SortedList s = new SortedList(onj);
+
+            foreach(DictionaryEntry d in s)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}", d.Key, d.Value);
+            }
+            */
+        }
+    }
+}
---
+++ Day2/Day2/Program12.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Day2
+{
+    class Goods
+    {
+        public int goodsno { get; set; }
+        public string gname { get; set; }
+        public int danga { get; set; }
+
+        public Goods(int goodsno, string gname, int danga)
+        {
+            this.goodsno = goodsno;
+            this.gname = gname;
+            this.danga = danga;
+        }
+
+        public String ToString()
+        {
+            return "Goods[상품번호: "+goodsno+", 상품명: "+gname+", 단가: "+danga+"]";
+        }
+    }
+
+    class Cart
+    {
+        public Goods goods { get; set; }
+        public int count { get; set; }
+        public int sum { get { return count * goods.danga; } set { } }
+
+        public Cart(Goods goods, int count)
+        {
+            this.goods = goods;
+            this.count = count;
+            sum = count * goods.danga;
+        }
+
+        public String ToString()
+        {
+            return "Cart[Goods: " + goods + ", count: " + count + ", sum: " + sum + "]";
+        }
+    }
+    
+    class Program12
+    {
+        static void Main()
+        {
+            Goods g1 = new Goods(1001, "볼펜", 1000);
+            Goods g2 = new Goods(1002, "연필", 500);
+            Goods g3 = new Goods(1003, "딸기", 6000);
+
+            Hashtable obj = new Hashtable();
+        }
+    }
+}
